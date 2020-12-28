086fcfc40879f3ad42374f346344826a50253bf7
dobrica996 <76686358+dobrica996@users.noreply.github.com>
2020-12-28 13:09:43 +0100

    Update login.php

--- citrussystems/login.php
+++ citrussystems/login.php
@@ -8,7 +8,7 @@ session_start();
 	<head>
 		<meta charset="utf-8" />
 		<title>Login</title>
-		    <meta name="description" content="prijava na sajt." />
+		    <meta name="description" content="Login" />
 
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 	</head>
